318f73ed3ac972b951f9e92653a66d5f034b1009
kaliztro <69448732+kaliztro@users.noreply.github.com>
2024-03-07 11:35:55 -0300

    finalizando o script
    Adicionado o teste de transferencia e a validação

--- src/test/java/MeuPrimeiroScriptAutomacaoJava.java
+++ src/test/java/MeuPrimeiroScriptAutomacaoJava.java
@@ -32,21 +32,16 @@ public class MeuPrimeiroScriptAutomacaoJava {
         driver.quit();
     }
 
+    private String modal;
     private String conta;
     private String conta2;
+
+    private String modal2;
     private String digito;
     private String digitoConta2;
 
-//    @Test
-//    public void acessarGoogle() {
-//        driver.get("https://www.google.com/");
-//        driver.findElement(By.id("APjFqb")).sendKeys("Unicred", Keys.ENTER);
-//    }
-
-
-
     @Test
-    public void criarConta() {
+    public void criarConta() throws InterruptedException {
 
         driver.get("https://bugbank.netlify.app/");
         driver.findElement(By.xpath("//button[normalize-space()='Registrar']")).click();
@@ -86,7 +81,7 @@ public class MeuPrimeiroScriptAutomacaoJava {
     }
 
     @Test
-    public void criarContaDois() {
+    public void criarContaDois() throws InterruptedException {
 
         driver.get("https://bugbank.netlify.app/");
         driver.findElement(By.xpath("//button[normalize-space()='Registrar']")).click();
@@ -95,6 +90,7 @@ public class MeuPrimeiroScriptAutomacaoJava {
         driver.findElement(By.xpath("(//input[@placeholder='Informe sua senha'])[2]")).sendKeys(senha2);
         driver.findElement(By.name("passwordConfirmation")).sendKeys(senha2);
 
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement((By.id("toggleAddBalance"))).click(); //conta com saldo
         driver.findElement(By.xpath("//button[normalize-space()='Cadastrar']")).click();
 
@@ -103,10 +99,15 @@ public class MeuPrimeiroScriptAutomacaoJava {
 
         String modalText2 = element.getText(); //pega o conteudo do modal
 
-        conta2 = modalText2.replaceAll("[^0-9]", ""); // Remove tudo que não for numero
-        System.out.println("Conta 2: " + conta2);
+        modal2 = modalText2.replaceAll("[^0-9]", ""); // Remove tudo que não for numero
+        System.out.println("Conta 2: " + modal2);
 
-        char ultimoCaractere = conta2.charAt(conta2.length() - 1); //pega apenas o ultimo caractere
+        //remover apenas o ultimo digito
+        String numeroConta2 = modal2.substring(0,modal2.length() - 1 );
+        conta2 = String.valueOf((numeroConta2));
+        System.out.println("Conta: " + conta2);
+
+        char ultimoCaractere = modal2.charAt(modal2.length() - 1); //pega apenas o ultimo caractere
         digitoConta2 = String.valueOf(ultimoCaractere); //variavel contendo o digito
         System.out.println("Ultimo numero: " + digitoConta2);
 
@@ -115,33 +116,54 @@ public class MeuPrimeiroScriptAutomacaoJava {
     }
 
     @Test
-    public void RealizarTransferencia(){
+    public void RealizarTransferencia() throws InterruptedException {
         criarConta();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(2000);
         acessarConta();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//a[@id='btn-TRANSFERÊNCIA']")).click();
 
 //        driver.findElement(By.id("btn-TRANSFERÊNCIA")).click(); //acessa o menu transferencia
-//        driver.findElement(By.name("accountNumber")).sendKeys(conta2);
-//        driver.findElement(By.name("digit")).sendKeys(digitoConta2);
-//        driver.findElement(By.name("transferValue")).sendKeys("500");
-//        driver.findElement(By.name("description")).sendKeys("Aqui está o dinheiro da Térvore");
-//
-//        driver.findElement(By.xpath("//button[normalize-space()='Transferir agora']")).click();
+        driver.findElement(By.name("accountNumber")).sendKeys(conta2);
+        driver.findElement(By.name("digit")).sendKeys(digitoConta2);
+        driver.findElement(By.name("transferValue")).sendKeys("500");
+        driver.findElement(By.name("description")).sendKeys("Aqui está o dinheiro da Térvore");
+
+        driver.findElement(By.xpath("//button[normalize-space()='Transferir agora']")).click();
+    }
+
+    public void acessarConta2() throws InterruptedException {
+
+        driver.get("https://bugbank.netlify.app/");
+
+        driver.findElement(By.name("email")).sendKeys(email2);
+        driver.findElement(By.name("password")).sendKeys(senha2);
+
+        driver.findElement(By.xpath(" //button[normalize-space()='Acessar']")).click(); //acessando a conta
+
+        Thread.sleep(1000);
+
+        String saldo = driver.findElement(By.id("textBalance")).getText(); //pega o saldo em conta
+        String saldoEmConta = saldo.replaceAll("[^0-9]", ""); //pega apenas o valor do texto
+
+        if(saldoEmConta.equals("150000")){
+            System.out.println("Transferencia realizada com sucesso"); //valida se a transferencia foi realizada
+        }
     }
 
 
     @Test
-    public void ExecutarFluxo(){
+    public void ExecutarFluxo() throws InterruptedException {
         criarConta();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(2000);
         criarContaDois();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(2000);
         acessarConta();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(2000);
         RealizarTransferencia();
+        Thread.sleep(2000);
+        acessarConta2();
     }
 
 }
